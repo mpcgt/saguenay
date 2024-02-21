bb3a19e391867d0db7441d7ee19e097befe6ceb0
Max <max.pcgt@gmail.com>
2024-02-21 21:07:08 +0100

    Updates files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Saguenay',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -50,7 +50,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: 'Soon',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
