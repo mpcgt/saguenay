74a75d91866b8a8dc7ceac20adc1297112873f11
Max <max.pcgt@gmail.com>
2024-10-07 00:50:35 +0200

    🐛 fix: Updates server - v.0.2.7

--- src/utils/supabaseClient.tsx
+++ src/utils/supabaseClient.tsx
@@ -4,7 +4,10 @@ function isString(value: string | undefined): value is string {
     return typeof value === 'string';
 }
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(
-    isString(import.meta.env.VITE_SUPABASE_URL) ? import.meta.env.VITE_SUPABASE_URL : 'REACT_APP_SUPABASE_URL',
-    isString(import.meta.env.VITE_SUPABASE_ANON_KEY) ? import.meta.env.VITE_SUPABASE_ANON_KEY : 'REACT_APP_SUPABASE_ANON_KEY'
+    isString(supabaseUrl) ? supabaseUrl : 'Invalid Supabase URL',
+    isString(supabaseAnonKey) ? supabaseAnonKey : 'Invalid Supabase Anon Key'
 );
